c0ff62182ac04e1e1803eeeb665d28e5ea1ce37f
Dr. Sergey Pogodin <dr.pogodin@gmail.com>
2019-11-09 21:20:04 +0100

    [#3] Polyfills `Promise.allSettled(..)`

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
+/* TODO: This is a temporary polyfill for `Promise.allSettled(..)` method,
+ * which is supported natively by NodeJS >= v12.9.0. As earlier NodeJS version
+ * are still in a wide use, this polyfill is added here, and it is to be dropped
+ * some time later. */
+if (!Promise.allSettled) {
+  Promise.allSettled = (promises) => Promise.all(
+    promises.map((p) => (p instanceof Promise ? p.finally(() => null) : p)),
+  );
+}
+
 export { default as GlobalStateProvider } from './GlobalStateProvider';
 export { default as useAsyncData } from './useAsyncData';
 export { default as useGlobalState } from './useGlobalState';
