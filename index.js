840c41afa5432b677764cdd525eade0a16dc113e
Dr. Sergey Pogodin <dr.pogodin@gmail.com>
2021-10-14 18:01:27 +0200

    Patched entry point: fallback to web build if node one cannot be loaded

--- index.js
+++ index.js
@@ -5,6 +5,14 @@
 
 /* eslint-disable global-require, no-eval */
 
-module.exports = typeof process !== 'undefined'
-  && process.versions && process.versions.node
-  ? eval('require')('./build/node') : require('./build/web');
+let lib;
+
+try {
+  lib = process.versions.node && eval('require')('./build/node');
+} catch (error) {
+  lib = undefined;
+}
+
+if (!lib) lib = require('./build/web');
+
+module.exports = lib;
