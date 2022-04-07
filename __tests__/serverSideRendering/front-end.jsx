ed6f40deac3f938c842e67a06b7997fd1577ac4c
Dr. Sergey Pogodin <dr.pogodin@gmail.com>
2022-04-07 22:56:10 +0200

    WIP: Updating SSR tests

--- __tests__/serverSideRendering/front-end.jsx
+++ __tests__/serverSideRendering/front-end.jsx
@@ -1,7 +1,7 @@
 /** @jest-environment jsdom */
 
-// ^^^ although it is strange to do SSR test in JSDom environment, the current
-// test organization requires it to verify results of SSR at client side.
+// ^^^ although it is strange to do SSR test in JSDom environment,
+// some of the tests should be done at the client side.
 
 /**
  * Base test of the server-side rendering features.
@@ -12,56 +12,23 @@ import pretty from 'pretty';
 
 let JU;
 let LIB;
-let ReactDOM;
 
 jest.mock('uuid');
 
 jest.useFakeTimers();
 mockdate.set('2019-11-07Z');
 
-// TODO: Are we missing something here? Right now the test components A and B,
-// look identical along with their loaders. Were they intended to be slightly
-// different to test different SSR corner cases?
-
-const loaderA = jest.fn(async () => {
-  await JU.timer(1000);
-  return 'data';
-});
-
-const loaderB = jest.fn(async () => {
-  await JU.timer(1000);
-  return 'data';
-});
-
-function ComponentA() {
-  const envelop = LIB.useAsyncData('x', loaderA);
-  return <div>{JSON.stringify(envelop, null, 2)}</div>;
-}
-
-function ComponentB() {
-  const envelop = LIB.useAsyncData('x', loaderB);
-  return <div>{JSON.stringify(envelop, null, 2)}</div>;
-}
-
-function Scene() {
-  const [globalValue] = LIB.useGlobalState('value.path', 'defaultValue');
-  return (
-    <div>
-      <h1>{globalValue}</h1>
-      <ComponentA />
-      <ComponentB />
-    </div>
-  );
-}
-
+let loaderA;
+let loaderB;
+let Scene;
 let scene;
 
 beforeEach(() => {
   jest.resetModules();
   delete process.env.REACT_GLOBAL_STATE_DEBUG;
-  ReactDOM = require('react-dom/server');
   JU = require('jest/utils');
   JU.unMockConsoleLog();
+  ({ default: Scene, loaderA, loaderB } = require('./__assets__/TestScene'));
   loaderA.mockClear();
   loaderB.mockClear();
 });
@@ -102,56 +69,6 @@ test('Scene test in the front-end mode', async () => {
   expect(pretty(scene.innerHTML)).toMatchSnapshot();
   expect(loaderA).toHaveBeenCalledTimes(1);
   expect(loaderB).toHaveBeenCalledTimes(0);
-});
-
-describe('React 17 style (renderToString())', () => {
-  test('Naive server-side rendering', () => {
-    LIB = require('src');
-    const render = ReactDOM.renderToString((
-      <LIB.GlobalStateProvider>
-        <Scene />
-      </LIB.GlobalStateProvider>
-    ));
-    expect(pretty(render)).toMatchSnapshot();
-  });
-
-  /**
-   * This is the sample SSR code assembly.
-   */
-  async function serverSideRender() {
-    LIB = require('src');
-    let render;
-    serverSideRender.round = 0;
-    const ssrContext = { state: {} };
-    for (; serverSideRender.round < 10; serverSideRender.round += 1) {
-      /* eslint-disable no-await-in-loop */
-      render = ReactDOM.renderToString((
-        <LIB.GlobalStateProvider
-          initialState={ssrContext.state}
-          ssrContext={ssrContext}
-        >
-          <Scene />
-        </LIB.GlobalStateProvider>
-      ));
-      if (ssrContext.dirty) {
-        await Promise.allSettled(ssrContext.pending);
-      } else break;
-      /* eslint-disable no-await-in-loop */
-    }
-    return render;
-  }
-
-  test('Smart server-sider rendering', async () => {
-    process.env.REACT_GLOBAL_STATE_DEBUG = true;
-    LIB = require('src');
-    JU.mockConsoleLog();
-    let render = serverSideRender();
-    await jest.runAllTimers();
-    render = await render;
-    expect(serverSideRender.round).toBe(1);
-    expect(render).toMatchSnapshot();
-    expect(console.log.logs).toMatchSnapshot();
-  });
 });
 
 describe('Test `getSsrContext()` function', () => {
