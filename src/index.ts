e1a7b8a8271970b3caee8372fc42a9e23e635774
Dr. Sergey Pogodin <dr.pogodin@gmail.com>
2023-06-26 12:05:24 +0200

    Misc updates

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,3 @@
-// TODO: This is a temporary polyfill for `Promise.allSettled(..)` method,
-// which is supported natively by NodeJS >= v12.9.0. As earlier NodeJS version
-// are still in a wide use, this polyfill is added here, and it is to be dropped
-// some time later.
-if (!Promise.allSettled) {
-  Promise.allSettled = (promises: Promise<any>[]) => Promise.all(
-    promises.map((p) => (p instanceof Promise ? p.finally(() => null) : p)),
-  );
-}
-
 export { default as SsrContext } from './SsrContext';
 
 export {
